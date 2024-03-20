fa2ca839835ca00cd6ffe9b1f0b3e7841ff68f4b
joablpaiva <joablpaiva@hotmail.com>
2024-03-20 19:25:43 -0300

    adicionado novos gráficos
    novos gráficos com totais de vendas, custos e lucro

--- trabalho_apresentação/1-Dashboard-Tatico/Demonstrativo-vendas.py
+++ trabalho_apresentação/1-Dashboard-Tatico/Demonstrativo-vendas.py
@@ -6,15 +6,29 @@ import streamlit as st
 import pandas as pd 
 import plotly.express as px
 
-st.markdown("# Vendas de veiculos") # ao usar # antes do texto ele ficará em destaque .
+
+# Configurando a página no formato landing page
+st.set_page_config(layout="wide")
+
+
+
+st.markdown("# Vendas de Veiculos de Luxo ") # ao usar # antes do texto ele ficará em destaque .
 
 # Carregando os dados, no caso a seguir um arquivo csv.
 df = pd.read_csv("vendasveiculo.csv",  encoding = "ISO-8859-1", sep=';')
 df
 
+#Adicionando abas para melhorar a visualização dos dados
+#aba1, aba2, aba3 = st.tabs(['Dataset', 'Receita', 'Vendedores'])
+
+
+
+
 # Opções de gráficos/pela barra lateral podemos escolher o gráfico a ser exibido 
 opcoes_graficos = ['Gráfico de Barras', 'Gráfico de Pizza']
 
+# Título para a seleção do ano na barra lateral
+st.sidebar.title('Selecione o Gráfico Desejado')
 
 # Barra lateral para seleção do tipo de gráfico
 selecao_grafico = st.sidebar.selectbox('Selecione o tipo de gráfico', opcoes_graficos)
@@ -51,3 +65,76 @@ if selecao_grafico == 'Gráfico de Barras':
     mostrar_grafico_barras()
 elif selecao_grafico == 'Gráfico de Pizza':
     mostrar_grafico_pizza()
+
+
+# Converter a coluna 'DataNotaFiscal' para o tipo datetime especificando o formato
+df['DataNotaFiscal'] = pd.to_datetime(df['DataNotaFiscal'], format='%d/%m/%Y')
+
+# Extrair o mês da coluna 'DataNotaFiscal' e criar uma nova coluna 'Mês'
+df['Mês'] = df['DataNotaFiscal'].dt.month
+
+# Título para a seleção do ano na barra lateral
+st.sidebar.title('Selecione o ano e veja a evolução de vendas ')
+
+# Adicionando a seleção do ano na barra lateral
+ano_selecionado = st.sidebar.selectbox('Selecione o ano:', sorted(df['Ano'].unique()))
+
+
+
+# Filtrar os dados pelo ano selecionado
+df_ano_selecionado = df[df['Ano'] == ano_selecionado]
+
+# Agrupar os dados filtrados por mês e calcular a soma das vendas para cada mês
+vendas_por_mes = df_ano_selecionado.groupby('Mês')['ValorVenda'].sum().reset_index()
+
+# Plotar o gráfico de linha da evolução das vendas por mês no ano selecionado
+fig_vendas_por_mes = px.line(vendas_por_mes, x='Mês', y='ValorVenda', title=f'Evolução das Vendas por Mês em {ano_selecionado}')
+st.plotly_chart(fig_vendas_por_mes)
+
+#############################################################################
+
+
+
+
+# Calcular a soma do custo de mão de obra para cada fabricante
+custo_mao_de_obra_por_fabricante = df.groupby('Fabricante')['CustoMaoDeObra'].sum().reset_index()
+
+# Ordenar os valores pelo custo de mão de obra em ordem decrescente
+custo_mao_de_obra_por_fabricante = custo_mao_de_obra_por_fabricante.sort_values(by='CustoMaoDeObra', ascending=False)
+
+# Criar um gráfico de barras com os totais de mão de obra para cada fabricante
+fig = px.bar(custo_mao_de_obra_por_fabricante, x='Fabricante', y='CustoMaoDeObra', title='Custo de Mão de Obra por Fabricante')
+st.plotly_chart(fig)
+
+
+#################################################################################
+
+
+
+# Calcular o lucro para cada venda
+df['Lucro'] = df['ValorVenda'] - (df['ValorCusto'] + df['TotalDesconto'] + df['CustoEntrega'] + df['CustoMaoDeObra'])
+
+# Agrupar por fabricante e somar os lucros
+lucro_por_fabricante = df.groupby('Fabricante')['Lucro'].sum().reset_index()
+
+# Classificar os fabricantes pelo lucro total
+lucro_por_fabricante = lucro_por_fabricante.sort_values(by='Lucro', ascending=False)
+
+# Criar um gráfico de barras com os lucros por fabricante
+fig = px.bar(lucro_por_fabricante, x='Fabricante', y='Lucro', title='Lucro por Fabricante')
+st.plotly_chart(fig)
+
+
+
+#################################################################################
+
+
+# Calcular o total de descontos para cada marca
+total_descontos_por_fabricante = df.groupby('Fabricante')['TotalDesconto'].sum().reset_index()
+
+# Classificar os fabricantes com base no total de descontos
+total_descontos_por_fabricante = total_descontos_por_fabricante.sort_values(by='TotalDesconto', ascending=False)
+
+# Criar um gráfico de barras com os descontos por fabricante
+fig = px.bar(total_descontos_por_fabricante, x='Fabricante', y='TotalDesconto', title='Total de Descontos por Fabricante')
+st.plotly_chart(fig)
